68e9dd8e894401076675df2fa1e839c98b32abff
veekR <victor1213113@gmail.com>
2024-12-13 13:42:30 -0800

    added data entitys and relations will link data set in git hub

---
+++ EvApp.Server/Data/Models/brand.cs
@@ -0,0 +1,34 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace EvApp.Server.Data.Models
+{
+    public class brand
+    {
+        #region Properties
+        /// <summary>
+        /// The unique id and primary key for this Country
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        /// <summary>
+        /// Country name (in UTF8 format)
+        /// </summary>
+        public required string BrandName { get; set; }
+        /// <summary>
+        /// Country code (in ISO 3166-1 ALPHA-2 format)
+        /// </summary>
+        public required int  Efficeincy { get; set; }
+        /// <summary>
+        /// Country code (in ISO 3166-1 ALPHA-3 format)
+        /// </summary>
+        public required string  PowerTrain{ get; set; }
+        #region Navigation Properties
+        /// <summary>
+        /// A collection of all the cities related to this country.
+        /// </summary>
+        public ICollection<Model>? models { get; set; }
+        #endregion
+    }
+}
---
+++ EvApp.Server/Data/Models/model.cs
@@ -0,0 +1,43 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Metrics;
+namespace EvApp.Server.Data.Models
+{
+    public class Model
+    {
+        #region Properties
+        /// <summary>
+        /// The unique id and primary key for this City
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        /// <summary>
+        /// City name (in UTF8 format)
+        /// </summary>
+        public required string ModelName { get; set; }
+        /// <summary>
+        /// City latitude
+        /// </summary>
+        [Column(TypeName = "decimal(7,4)")]
+        public int TopSpeed { get; set; }
+        /// <summary>
+        /// City longitude
+        /// </summary>
+        [Column(TypeName = "decimal(7,4)")]
+        public int MileRange { get; set; }
+        /// <summary>
+        /// Country Id (foreign key)
+        /// </summary>
+        [ForeignKey(nameof(brand))] 
+        public int BrandId { get; set; }
+        #endregion
+        #region Navigation Properties
+        /// <summary>
+        /// The country related to this city.
+        /// </summary>
+        public brand? brand { get; set; }
+        #endregion
+    }
+}
