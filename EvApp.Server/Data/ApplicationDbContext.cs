f28d4e9cc2277264952d828477b94e87e0dc2d9a
veekR <victor1213113@gmail.com>
2024-12-13 14:24:58 -0800

    added configs and server updates

--- EvApp.Server/Data/ApplicationDbContext.cs
+++ EvApp.Server/Data/ApplicationDbContext.cs
@@ -2,18 +2,55 @@
 using System.Diagnostics.Metrics;
 using Microsoft.EntityFrameworkCore;
 using EvApp.Server.Data.Models;
+
 namespace EvApp.Server.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        // Default constructor
         public ApplicationDbContext() : base()
         {
         }
-        public ApplicationDbContext(DbContextOptions options)
-         : base(options)
+
+        // Constructor with DbContext options
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
         }
-        public DbSet<Model> models => Set<Model>();
-        public DbSet<Brand> brands=> Set<Brand>();
+
+        // DbSets
+        public DbSet<Model> Models => Set<Model>();
+        public DbSet<Brand> Brands => Set<Brand>();
+
+        // OnModelCreating method
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure Model entity
+            modelBuilder.Entity<Model>().ToTable("Models");
+            modelBuilder.Entity<Model>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<Model>()
+                .Property(x => x.Id).IsRequired();
+            modelBuilder.Entity<Model>()
+                .Property(x => x.TopSpeed).HasColumnType("decimal(7,4)");
+            modelBuilder.Entity<Model>()
+                .Property(x => x.MileRange).HasColumnType("decimal(7,4)");
+
+            // Configure Brand entity
+            modelBuilder.Entity<Brand>().ToTable("Brands");
+            modelBuilder.Entity<Brand>()
+                .HasKey(x => x.Id);
+            modelBuilder.Entity<Brand>()
+                .Property(x => x.Id).IsRequired();
+
+            // Configure relationship between Model and Brand
+            modelBuilder.Entity<Model>()
+                .HasOne(x => x.brand)
+                .WithMany(y => y.models)
+                .HasForeignKey(x => x.BrandId);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
     }
 }
---
+++ EvApp.Server/Data/Models/CountryEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Diagnostics.Metrics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace EvApp.Server.Data.Models
+{
+    public class CountryEntityTypeConfiguration
+        : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.ToTable("Brands");
+            builder.HasKey(x => x.Efficeincy);
+            builder.Property(x => x.PowerTrain).IsRequired();
+        }
+    }
+}
